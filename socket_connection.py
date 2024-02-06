ed8eee218d2acf3b62a9d2dc80ec9d23c5572a18
Marit <93144973+tiram-isu@users.noreply.github.com>
2024-02-06 19:26:51 +0100

    Update socket_connection.py

--- socket_connection.py
+++ socket_connection.py
@@ -21,24 +21,3 @@ class UnityClient:
         self.sock.close()
         if self.sock:
             self.sock = None
-
-
-
-
-# host, port = "127.0.0.1", 13000
-# data = ["Beatiful day in the neighborhood", "a beautiful day if a neighbor could", "won't you be mine?"]
-
-# # SOCK_STREAM means TCP socket
-# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# try:
-#     # Connect to the server and send the data
-#     sock.connect((host, port))
-#     sock.sendall(data[0].encode("utf-8"))
-#     sock.sendall(data[1].encode("utf-8"))
-#     sock.sendall(data[2].encode("utf-8"))
-#     # response = sock.recv(1024).decode("utf-8")
-#     # print (response)
-
-# finally:
-#     sock.close()
